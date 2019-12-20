fd28d3068094b43a52b380898ff4654ebe3e945b
Tarek El-Hajjaoui <tarekel96@gmail.com>
2019-12-19 20:22:40 -0800

    added underline feature that creates the amount of underlines equal to the random word that is chosen and added an input box where the client can input their letter guess and fixed up the visual spacing margins and font size

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,35 @@
-$(document).ready(function() {
-  $("#about-btn").click(function() {
-    console.log("here");
-    $("#about").toggle();
-  });
+let wordList = ["apple", "orange", "banana", "pear"],
+  mysteryWord = document.querySelector("#mystery-word"),
+  aboutButton = document.querySelector("#about-btn"),
+  aboutInfo = document.querySelector("#about"),
+  inputWord = document.querySelector("#input-word"),
+  letterArray = mysteryWord.children;
+
+aboutButton.addEventListener("click", function() {
+  if (aboutInfo.hidden == true) {
+    aboutInfo.hidden = false;
+  } else if (aboutInfo.hidden == false) {
+    aboutInfo.hidden = true;
+  }
 });
+
+function displayUnderlines() {
+  let chosen = wordList[Math.floor(Math.random() * (wordList.length - 1))];
+  console.log(chosen);
+  for (letter of chosen) {
+    let node = document.createElement("SPAN");
+    node.innerHTML = " _ ";
+    mysteryWord.appendChild(node);
+  }
+  console.log(letterArray[0]);
+}
+
+displayUnderlines();
+
+// function randomWord() {
+//   let chosen = wordList[Math.floor(Math.random() * (wordList.length - 1))];
+//   console.log(chosen);
+//   return chosen;
+// }
+
+// randomWord();
