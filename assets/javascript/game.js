71fc7c86e4d5e85e26c7ea57dbaee8ad3d6f3b49
Tarek El-Hajjaoui <tarekel96@gmail.com>
2019-12-31 12:31:26 -0800

    cleaned up the code which creates the random word and displays the mystery lines and made it more visually appealing started to work on the logic that will replace the letter with the correctly guessed letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ let wordList = ["apple", "orange", "banana", "pear"],
   inputWord = document.querySelector("#input-word"),
   letterArray = mysteryWord.children;
 
+// toggles the about info section to display and not to display
 aboutButton.addEventListener("click", function() {
   if (aboutInfo.hidden == true) {
     aboutInfo.hidden = false;
@@ -13,27 +14,48 @@ aboutButton.addEventListener("click", function() {
   }
 });
 
+// random word chosen for user to guess - HOW IT WORKS:
+let chosen = wordList[Math.floor(Math.random() * (wordList.length - 1))];
+console.log(chosen);
+
+//
 function displayUnderlines() {
-  let chosen = wordList[Math.floor(Math.random() * (wordList.length - 1))];
-  console.log(chosen);
   for (letter of chosen) {
-    let nodeDiv = document.createElement("DIV");
     let nodeSpan = document.createElement("SPAN");
 
-    nodeSpan.innerHTML = letter;
-    nodeDiv.innerHTML = "_";
+    nodeSpan.innerHTML = " - ";
     mysteryWord.appendChild(nodeSpan);
-    nodeSpan.appendChild(nodeDiv);
   }
   console.log(letterArray[0]);
 }
 
 displayUnderlines();
 
-// function randomWord() {
-//   let chosen = wordList[Math.floor(Math.random() * (wordList.length - 1))];
-//   console.log(chosen);
-//   return chosen;
-// }
+function checkGuess() {
+  // user inputs a letter
 
-// randomWord();
+  for ([letter, index] of chosen) {
+    if (letter === inputWord) {
+      let correctGuess = letter;
+      let correctPos = index;
+    }
+  }
+}
+
+checkGuess();
+
+// user inputs a letter
+
+// check if letter is in the chosen word - for loop
+
+// if letter matches a letter in the chosen word, turn the letter in word to a variable
+
+// replace the '-' value with the correctly guessed letter so the user can see the progress
+
+//   let con = true
+
+//   do {
+
+//   }
+
+//   while(con === true)
